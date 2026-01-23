0aa73ce444ecca66358b6cb461ffdc104f32cc99
Siddharth Malviya <sidmlv43@gmail.com>
2026-01-23 17:04:54 +0530

    added test cases and minor bug fix

--- src/test/java/com/comcast/testcases/ProductTests.java
+++ src/test/java/com/comcast/testcases/ProductTests.java
@@ -1,17 +1,18 @@
 package com.comcast.testcases;
 
 import com.comcast.helpers.Helpers;
+import com.comcast.payloads.Payload;
 import com.comcast.pojo.Product;
 import com.comcast.routes.Routes;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.*;
 
 public class ProductTests extends BaseClass{
 
@@ -95,6 +96,60 @@ public class ProductTests extends BaseClass{
                 .getList(".", Product.class);
 
         Assert.assertTrue(Helpers.isSorted(products, Product::getId, false));
+    }
+
+
+
+    @Test(testName = "Get All Categories")
+    public void testGetAllCategories() {
+        given()
+        .when()
+                .get(Routes.GET_ALL_CATEGORIES)
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThan(0))
+//                .body(contains("catego"))
+                .log().all()
+        ;
+    }
+
+    @Test (testName = "Get Product By category")
+    public void testGetProductByCategory() {
+        Response res = given()
+                .pathParams("category", "electronics")
+        .when()
+                .get(Routes.GET_PRODUCT_BY_CATEGORY)
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("size()", greaterThan(0))
+                .extract().response()
+//                .log().all()
+        ;
+
+        List<String> categories = res.jsonPath().getList("category");
+        boolean allMatched = categories.stream().allMatch(t-> t.equals("electronics"));
+        Assert.assertTrue(allMatched, "expected only electronics but found others as well");
+    }
+
+
+    @Test(testName = "Create Product Test")
+    public void testCreateProduct(){
+        Product product = Payload.getProductPayload();
+        int id = given()
+                .contentType(ContentType.JSON)
+                .body(product)
+        .when().post(Routes.CREATE_PRODUCT)
+        .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("category", equalTo(product.getCategory()))
+//                .body("price", product.getPrice())
+                .extract().jsonPath().getInt("id");
+
+        System.out.println(id);
     }
 
 }

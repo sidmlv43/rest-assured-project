6f788b1343c598cf7727be539bf2c0e6f3df7980
Siddharth Malviya <sidmlv43@gmail.com>
2026-01-26 12:03:10 +0530

    add Data Driven Testing test cases

--- src/test/java/com/comcast/testcases/ProductDataDrivenTest.java
+++ src/test/java/com/comcast/testcases/ProductDataDrivenTest.java
@@ -1,8 +1,15 @@
 package com.comcast.testcases;
 
+import com.comcast.pojo.Product;
+import com.comcast.routes.Routes;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class ProductDataDrivenTest extends BaseClass{
 
@@ -13,7 +20,32 @@ public class ProductDataDrivenTest extends BaseClass{
             dataProviderClass = com.comcast.utils.DataProviders.class
     )
     public void testAddNewProduct(Map<String, String>  data ) {
-        System.out.println(data);
+        String title = data.get("title");
+        double price = Double.parseDouble(data.get("price"));
+        String category = data.get("category");
+        String image = data.get("image");
+        String description = data.get("description");
+        Product prod = Product.builder()
+                        .title(title)
+                                .price(price)
+                                        .category(category)
+                                                .description(description)
+                                                        .image(image)
+                                                                .build();
+
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(prod)
+
+        .when()
+                .post(Routes.CREATE_PRODUCT)
+        .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("title", equalTo(prod.getTitle()))
+        ;
     }
 
 }
